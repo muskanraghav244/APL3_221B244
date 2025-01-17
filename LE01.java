6fe0c5b73d94776109a5a5b6d5e77961a9a9afae
221B244 <221B244@juet.co.in>
2025-01-17 14:53:55 +0530

    Lab exercise 1

---
+++ LE01.java
@@ -0,0 +1,31 @@
+class Student {
+    String name;
+    int age;
+    
+    void setName(String name) {
+        this.name = name;
+    }
+    
+    void setAge(int age) {
+        this.age = age;
+    }
+    
+    String getName() {
+        return name;
+    }
+    
+    int getAge() {
+        return age;
+    }
+}
+
+public class LE01
+{
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		s1.setName("John");
+		s1.setAge(21);
+		System.out.println(s1.getName());
+		System.out.println(s1.getAge());
+	}
+}
